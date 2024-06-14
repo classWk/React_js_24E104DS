296681a756fd433ddce692b228c3da6955d1c1e4
classWk <ksingh@sevenmentor.com>
2024-06-14 14:54:30 +0530

    new

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
 import './card.css'
-function Card() {
+import { Link } from 'react-router-dom'
+function Card({item}) {
   return (
-    <div>Card</div>
+    <div className="card">
+        <Link to={`/${item.id}`} className='imageContainer'>
+            <img src={item.img} alt="" />
+        </Link>
+        <div className="textContainer">
+            <h2 className='title'>
+                <Link to={`/${item.id}`}>{item.title}</Link>
+            </h2>
+            <p className="address">
+                <img src="/public/pin.png" alt="" />
+                <span>{item.address}</span>
+            </p>
+            <p className="price">
+            ₹{item.price}
+            </p>
+            <div className="bottom">
+                <div className="features">
+                    <div className="feature">
+                        <img src="/public/bed.png" alt="" />
+                        <span>{item.bedroom} bedroom</span>
+                    </div>
+                    <div className="feature">
+                        <img src="/public/bath.png" alt="" />
+                        <span>{item.bedroom} bathroom</span>
+                    </div>
+                </div>
+                <div className="icons">
+                    <div className="icon">
+                        <img src="/public/save.png" alt="" />
+                    </div>
+                    <div className="icon">
+                        <img src="/public/chat.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -2,7 +2,70 @@ import React from 'react'
 import './filter.css'
 function Filter() {
   return (
-    <div>Filter</div>
+    <div className="filter">
+      <h1>Search results for <b>Pune</b></h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor="city">Pune</label>
+          <input 
+          type="text"
+          id='city'
+          name='city'
+          placeholder='City Loaction'
+          />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type">
+            <option value="">any</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select name="property" id="property">
+            <option value="">any</option>
+            <option value="apartment">Apartment</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="land">Land</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice">Min Price</label>
+          <input 
+          type="number" 
+          id='minPrice'
+          name='minPrice'
+          placeholder='any'
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input 
+          type="text" 
+          id='maxPrice'
+          name='maxPrice'
+          placeholder='any'
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom">Bedroom</label>
+          <input 
+          type="text"
+          id='bedroom'
+          name='bedroom'
+          placeholder='any'
+          />
+        </div>
+        <button>
+          <img src="/public/search.png" alt="" />
+        </button>
+      </div>
+    </div>
   )
 }
 export default Filter
